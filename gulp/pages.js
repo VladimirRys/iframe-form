1bcfb9f16fa2f3e5181570c5f3b905b11aad4f58
Arthur Bailyes Li <arthurlitovko@gmail.com>
2021-06-07 16:40:12 +0300

    Merge json task

--- gulp/pages.js
+++ gulp/pages.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import pug from "gulp-pug";
 import pugbem from "gulp-pugbem";
 
+import mergeJSON from "gulp-merge-json";
+
 // import data from "gulp-data";
 import fs from "fs";
 
@@ -9,21 +11,24 @@ import { Path } from "./_const.js";
 
 const isPretty = process.env.NODE_ENV === "development" ? true : false;
 
+export const pages = gulp.series(prepareData, compilePug);
 
-export function pages() {
+function prepareData() {
+	return gulp
+		.src(Path.DATA.source + "*.json")
+		.pipe(mergeJSON())
+		.pipe(gulp.dest("./temp"));
+}
+
+function compilePug() {
 	return gulp
 		.src(Path.PAGE.source)
 		.pipe(
 			pug({
 				pretty: isPretty,
 				plugins: [pugbem],
-				data: prepareData(),
+				data: JSON.parse(fs.readFileSync("./temp/combined.json")),
 			})
 		)
 		.pipe(gulp.dest(Path.PAGE.build));
-}
-
-
-function prepareData() {
-	return JSON.parse(fs.readFileSync(Path.DATA.source + "data.json"));
 }
