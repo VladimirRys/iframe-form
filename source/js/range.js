1f19a54063b0877c11875c489bdb861185d4f1dd
Arthur Bailyes Li <arthurlitovko@gmail.com>
2021-06-07 18:38:16 +0300

    Refactoring (delete imask)

--- source/js/range.js
+++ source/js/range.js
@@ -1,33 +1,42 @@
-import IMask from "imask";
 import { updateByTraffic } from "./calculator.js";
 
 const rangeWrapper = document.querySelector(".range-slider");
 const range = rangeWrapper.querySelector("input");
 const inputNumber = document.getElementById("traffic");
 
-
 updateByTraffic(range.value);
-
-const numberMask = IMask(inputNumber, {
-	mask: Number,
-	min: rangeWrapper.style.getPropertyValue("--min"),
-	max: rangeWrapper.style.getPropertyValue("--max"),
-	thousandsSeparator: " ",
-});
 
 range.addEventListener("input", () => {
 	rangeWrapper.style.setProperty("--value", range.value);
 });
 
 range.addEventListener("change", () => {
-	inputNumber.value = range.value;
-	numberMask.updateValue(range.value);
-	inputNumber.value = numberMask.value;
+	inputNumber.value = formatNumber(range.value);
 	updateByTraffic(range.value);
 });
 
 inputNumber.addEventListener("input", () => {
-	range.value = numberMask.unmaskedValue;
-	rangeWrapper.style.setProperty("--value", range.value);
-	updateByTraffic(range.value);
+	let newValue = removeSpacesFromNumber(inputNumber.value);
+
+	if (isNaN(newValue)) {
+		newValue = 0;
+	}
+
+	if (newValue > range.max) {
+		newValue = range.max;
+	}
+
+	inputNumber.value = formatNumber(newValue);
+
+	range.value = newValue;
+	rangeWrapper.style.setProperty("--value", newValue);
+	updateByTraffic(newValue);
 });
+
+function formatNumber(number) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function removeSpacesFromNumber(string) {
+	return parseInt(string.replace(/[^0-9.]/g, ""));
+}
